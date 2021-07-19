fba0c65a8cfbac097694ecdb4fb1c246e399d87e
Sanjay Vasandani <sanjayvas@google.com>
2021-07-19 09:42:32 -0700

    Add extension functions for creating and verifying digital signatures.
    These utilize X.509 certificates.

---
+++ src/test/kotlin/org/wfanet/measurement/consent/crypto/SignaturesTest.kt
@@ -0,0 +1,84 @@
+// Copyright 2021 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.wfanet.measurement.consent.crypto
+
+import com.google.common.collect.Range
+import com.google.common.truth.Truth.assertThat
+import com.google.protobuf.ByteString
+import java.nio.file.Paths
+import java.security.cert.X509Certificate
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.wfanet.measurement.common.crypto.readCertificate
+import org.wfanet.measurement.common.crypto.readPrivateKey
+import org.wfanet.measurement.common.getRuntimePath
+
+private const val KEY_ALGORITHM = "EC"
+private val DATA = ByteString.copyFromUtf8("I am some data to sign")
+private val ALT_DATA = ByteString.copyFromUtf8("I am some alternative data")
+private val TESTDATA_DIR =
+  Paths.get(
+    "wfa_common_jvm",
+    "src",
+    "main",
+    "kotlin",
+    "org",
+    "wfanet",
+    "measurement",
+    "common",
+    "crypto",
+    "testing",
+    "testdata"
+  )
+
+@RunWith(JUnit4::class)
+class SignaturesTest {
+  @Test
+  fun `sign returns signature of correct size`() {
+    val privateKey = readPrivateKey(SERVER_KEY_FILE, KEY_ALGORITHM)
+    val certificate: X509Certificate = readCertificate(SERVER_CERT_PEM_FILE)
+
+    val signature = privateKey.sign(certificate, DATA)
+
+    // DER-encoded ECDSA signature using 256-bit key can be 70, 71, or 72 bytes.
+    assertThat(signature.size()).isIn(Range.closed(70, 72))
+  }
+
+  @Test
+  fun `verifySignature returns true for valid signature`() {
+    val privateKey = readPrivateKey(SERVER_KEY_FILE, KEY_ALGORITHM)
+    val certificate: X509Certificate = readCertificate(SERVER_CERT_PEM_FILE)
+    val signature = privateKey.sign(certificate, DATA)
+
+    assertTrue(certificate.verifySignature(DATA, signature))
+  }
+
+  @Test
+  fun `verifySignature returns false for signature from different data`() {
+    val privateKey = readPrivateKey(SERVER_KEY_FILE, KEY_ALGORITHM)
+    val certificate: X509Certificate = readCertificate(SERVER_CERT_PEM_FILE)
+    val signature = privateKey.sign(certificate, DATA)
+
+    assertFalse(certificate.verifySignature(ALT_DATA, signature))
+  }
+
+  companion object {
+    private val SERVER_CERT_PEM_FILE = getRuntimePath(TESTDATA_DIR.resolve("server.pem"))!!.toFile()
+    private val SERVER_KEY_FILE = getRuntimePath(TESTDATA_DIR.resolve("server.key"))!!.toFile()
+  }
+}
