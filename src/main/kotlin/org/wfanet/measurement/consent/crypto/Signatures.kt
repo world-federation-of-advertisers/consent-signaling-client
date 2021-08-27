a0fb866a56d036caac4ad32aedee7d69f04b6635
Jon Molle <jmolle@google.com>
2021-08-26 17:14:39 -0700

    Add support for signing and validating Flows
    Used in panel exchange storage to sign and validate blob data. The storage
      extensions added in client allow for public access to the new signature
      functions.
    
    - The signature returns a pair and requires the flow to be completed for the
      signature to be generated
    
    - The validate step throws a custom DataFailsSignatureValidation if the Flow is
      invalid.
    
    - Adds public helper extension functions in storage/Storage.kt that support
      signature matching (Blob.verifiedRead and StorageClient.createSignedBlob).

--- src/main/kotlin/org/wfanet/measurement/consent/crypto/Signatures.kt
+++ src/main/kotlin/org/wfanet/measurement/consent/crypto/Signatures.kt
@@ -18,8 +18,17 @@ import com.google.protobuf.ByteString
 import java.security.PrivateKey
 import java.security.Signature
 import java.security.cert.X509Certificate
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.runBlocking
 import org.wfanet.measurement.api.v2alpha.SignedData
 import org.wfanet.measurement.common.crypto.jceProvider
+import org.wfanet.measurement.consent.crypto.exception.InvalidSignatureException
 
 /**
  * Signs [data] using this [PrivateKey].
@@ -27,10 +36,35 @@ import org.wfanet.measurement.common.crypto.jceProvider
  * @param certificate the [X509Certificate] that can be used to verify the signature
  */
 fun PrivateKey.sign(certificate: X509Certificate, data: ByteString): ByteString {
+  val deferredSig = this.signFlow(certificate, flow { emit(data) })
+  val signature: ByteString
+  runBlocking {
+    deferredSig.first.collect()
+    signature = deferredSig.second.await()
+  }
+  return signature
+}
+
+/**
+ * Signs [data] using this [PrivateKey]. Takes in a [Flow<ByteString>] for signing streaming data.
+ *
+ * Note that the deferred output (the signature) will only be ready when the Flow has been fully
+ * collected.
+ *
+ * @param certificate the [X509Certificate] that can be used to verify the signature
+ */
+fun PrivateKey.signFlow(
+  certificate: X509Certificate,
+  data: Flow<ByteString>
+): Pair<Flow<ByteString>, Deferred<ByteString>> {
   val signer = Signature.getInstance(certificate.sigAlgName, jceProvider)
+  val deferredSig = CompletableDeferred<ByteString>()
   signer.initSign(this)
-  signer.update(data.asReadOnlyByteBuffer())
-  return ByteString.copyFrom(signer.sign())
+  val outFlow =
+    data.onEach { signer.update(it.asReadOnlyByteBuffer()) }.onCompletion {
+      deferredSig.complete(ByteString.copyFrom(signer.sign()))
+    }
+  return outFlow to deferredSig
 }
 
 /**
@@ -38,10 +72,13 @@ fun PrivateKey.sign(certificate: X509Certificate, data: ByteString): ByteString 
  * [X509Certificate].
  */
 fun X509Certificate.verifySignature(data: ByteString, signature: ByteString): Boolean {
-  val verifier = Signature.getInstance(this.sigAlgName, jceProvider)
-  verifier.initVerify(this)
-  verifier.update(data.asReadOnlyByteBuffer())
-  return verifier.verify(signature.toByteArray())
+  val resultFlow = this.verifySignedFlow(flow { emit(data) }, signature, "File")
+  return try {
+    runBlocking { resultFlow.collect() }
+    true
+  } catch (e: InvalidSignatureException) {
+    false
+  }
 }
 
 /**
@@ -49,4 +86,27 @@ fun X509Certificate.verifySignature(data: ByteString, signature: ByteString): Bo
  */
 fun X509Certificate.verifySignature(signedData: SignedData): Boolean {
   return verifySignature(signedData.data, signedData.signature)
+}
+
+/**
+ * Verifies that the [signature] for the Flow [data] was signed by the entity represented by this
+ * [X509Certificate].
+ *
+ * The output is the downstream Flow of [data]. If [data] is found to not match [signature] upon
+ * collecting the flow, this will throw an [IllegalArgumentException].
+ */
+@Throws(InvalidSignatureException::class)
+fun X509Certificate.verifySignedFlow(
+  data: Flow<ByteString>,
+  signature: ByteString,
+  blobKey: String
+): Flow<ByteString> {
+  val verifier = Signature.getInstance(this.sigAlgName, jceProvider)
+
+  verifier.initVerify(this)
+  return data.onEach { verifier.update(it.asReadOnlyByteBuffer()) }.onCompletion {
+    if (!verifier.verify(signature.toByteArray())) {
+      throw InvalidSignatureException(blobKey)
+    }
+  }
 }
